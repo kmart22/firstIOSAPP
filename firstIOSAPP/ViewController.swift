32de23861c3f2277e2e34ad8f5d39cb5f9b046b3
kmart22 <kmar7070@740s-pl-111.local>
2016-09-26 08:15:57 -0600

    started repository. added slider

---
+++ firstIOSAPP/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  firstIOSAPP
+//
+//  Created by Martinsen, Kaden on 9/26/16.
+//  Copyright © 2016 Martinsen, Kaden. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
